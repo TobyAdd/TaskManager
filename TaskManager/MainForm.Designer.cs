7027f4f6fe571488256b4dc159aa149c76280d5b
TobyAdd <66429886+TobyAdd@users.noreply.github.com>
2022-05-12 15:28:04 +0300

    Add files via upload

---
+++ TaskManager/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace TaskManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProcessList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ProcessList
+            // 
+            this.ProcessList.BackColor = System.Drawing.Color.DimGray;
+            this.ProcessList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ProcessList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProcessList.ForeColor = System.Drawing.Color.White;
+            this.ProcessList.FormattingEnabled = true;
+            this.ProcessList.ItemHeight = 20;
+            this.ProcessList.Location = new System.Drawing.Point(12, 12);
+            this.ProcessList.Name = "ProcessList";
+            this.ProcessList.Size = new System.Drawing.Size(651, 302);
+            this.ProcessList.TabIndex = 0;
+            this.ProcessList.SelectedIndexChanged += new System.EventHandler(this.ProcessList_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(523, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kill Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(416, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Suspend";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(12, 320);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Update List";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(675, 352);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ProcessList);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "MainForm";
+            this.Text = "Task Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ProcessList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TaskManager/MainForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace TaskManager
+{
+    public partial class MainForm : Form
+    {
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+        List<string> Suspendlist = new List<string>();
+        bool killed;
+        public MainForm()
+        {
+            InitializeComponent();
+            ListProcess();
+        }
+        void ListProcess()
+        {
+            Process[] proceesList = Process.GetProcesses();
+            foreach (Process p in proceesList)
+            {
+                ProcessList.Items.Add(p.ProcessName);
+            }
+        }
+
+        void SuspendProcess(int pid)
+        {
+            var process = Process.GetProcessById(pid);
+            
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                SuspendThread(pOpenThread);
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        void ResumeProcess(int pid)
+        {
+            var process = Process.GetProcessById(pid);
+
+            if (process.ProcessName == string.Empty)
+                return;
+
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                var suspendCount = 0;
+                do
+                {
+                    suspendCount = ResumeThread(pOpenThread);
+                } while (suspendCount > 0);
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ProcessList.Items.Clear();
+            ListProcess();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            killed = true;
+            Process[] KillProcess = Process.GetProcessesByName(ProcessList.SelectedItem.ToString());
+
+            foreach (Process Kill in KillProcess)
+            {
+                Kill.Kill();
+                Kill.WaitForExit();
+                Kill.Dispose();
+                ProcessList.Items.Remove(ProcessList.SelectedItem);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var processid = Process.GetProcessesByName(ProcessList.SelectedItem.ToString());
+
+            if (button2.Text == "Suspend")
+            {           
+                foreach (var process in processid)
+                {
+                    SuspendProcess(process.Id);
+                    Suspendlist.Add(ProcessList.SelectedItem.ToString());
+                }
+                button2.Text = "Resume";
+            }
+            else
+            {
+                foreach (var process in processid)
+                {
+                    ResumeProcess(process.Id);
+                    Suspendlist.Remove(ProcessList.SelectedItem.ToString());
+                }
+                button2.Text = "Suspend";
+            }
+        }
+
+        private void ProcessList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (killed)
+            {
+                killed = false;
+            }
+            else
+            {                
+                if (Suspendlist.Contains(ProcessList.SelectedItem.ToString()))
+                {
+                    button2.Text = "Resume";
+                }
+                else
+                {
+                    button2.Text = "Suspend";
+                }
+            }
+
+
+        }
+    }
+}
